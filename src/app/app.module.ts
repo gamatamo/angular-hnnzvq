3d91a8b6ea31b305ed395689caf353552acc5531
gamatamo <gustavitomatamoros@gmail.com>
2019-11-25 02:20:15 -0500

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MainComponent } from './components/shared/main/main.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, MainComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
